c5ac5975ffa9e5a53ce17f64e258446050fc1429
Ruslan Lezhnin <ruslanlezhnin@users-Mac-mini.local>
2017-12-18 15:54:37 +0200

    Initial Commit

---
+++ StackOverflowReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackOverflowReader
+//
+//  Created by Ruslan Lezhnin on 12/18/17.
+//  Copyright © 2017 chisw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
