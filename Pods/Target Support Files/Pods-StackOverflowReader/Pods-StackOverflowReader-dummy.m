3859086c4c0748a4d34635f79f5076e561c676af
Ruslan Lezhnin <ruslanlezhnin@users-Mac-mini.local>
2017-12-22 15:42:22 +0200

    Installed Alamofire, CodableAlamofire. Proper project structure

---
+++ Pods/Target Support Files/Pods-StackOverflowReader/Pods-StackOverflowReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StackOverflowReader : NSObject
+@end
+@implementation PodsDummy_Pods_StackOverflowReader
+@end
--- StackOverflowReader/Controllers/ViewController.swift
+++ StackOverflowReader/Controllers/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import CodableAlamofire
 
 class ViewController: UIViewController {
 
